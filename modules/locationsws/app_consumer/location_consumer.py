6f7383dd37d29cb93e3cde1301e3198a5820d363
Gabriel Enriquez <gab_s@hotmail.com>
2023-05-05 10:46:06 -0400

    consumer logic done, needs to be containerized next

--- modules/locationsws/app_consumer/location_consumer.py
+++ modules/locationsws/app_consumer/location_consumer.py
@@ -1,21 +1,55 @@
+from typing import Optional
 from kafka import KafkaConsumer
-
-TOPIC_NAME = 'items'
-
-consumer = KafkaConsumer(TOPIC_NAME)
-for message in consumer:
-    #todo: recieve location, store location in un_process_location table/area/DB
-    #every 5 min (update timer accordingly), another process calcualtes Connections
-    #     and updates DB with connections table (as opposed to calculate per query ar runtime)
-    print (message)
+from bson import dumps, loads
 
 def create_kafka_consumer(topic_name:str):
     return KafkaConsumer(topic_name)
 
-class Consumer():
-    def __init__(self, kafka_consumer=create_kafka_consumer()) -> None:
-        pass
+class LocationConsumer():
+    def __init__(self, kafka_consumer=Optional[KafkaConsumer], location_dao=None) -> None:
+        self.kafka_consumer = kafka_consumer
+        self.location_dao = location_dao
 
-    def consumer(self) -> None:
-        pass
+    def consume(self) -> None:
+        for message in self.kafka_consumer:
+            self.location_dao.create(loads(message))
 
+def start():
+    location_consumer = LocationConsumer()
+    while True:
+        location_consumer.consume()
+
+if __name__ == "__main__":
+    print('testing consumer')
+    
+    loc_a = {
+        "person_id":2,
+        "coordinate":"coordinatexy"
+    }
+
+    loc_b = {
+        "person_id":3,
+        "coordinate":"coordinatexyz"
+    }
+    
+    locations = (
+        dumps(loc_a),
+        dumps(loc_b)
+    )
+    mocked_kafka_consumer = iter(locations)
+
+    class MockedLocationDao():
+        def __init__(self) -> None:
+            self.locations=[]
+
+        def create(self, loc):
+            self.locations.append(loc)
+
+    mocked_dao = MockedLocationDao()
+    test_consumer = LocationConsumer(kafka_consumer=mocked_kafka_consumer, location_dao=mocked_dao)
+    test_consumer.consume()
+
+    total_dao_sinked_locations = len(mocked_dao.locations)
+    print(f'msgs consumed total:{total_dao_sinked_locations}; locations:{mocked_dao.locations}')
+    if total_dao_sinked_locations != 2:
+        raise Exception(f"should have sent {len(locations)} locations to DAO")
